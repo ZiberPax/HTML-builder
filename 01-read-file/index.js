8d81e023b6b33cc8d5d5c410ee4d1e632dfda65a
Alexey Meshin <ou3giu13hg@gmail.com>
2023-05-04 21:53:18 +0300

    feat: finished task 1

---
+++ 01-read-file/index.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const path = require("path");
+
+let stream = new fs.ReadStream(path.join(__dirname, "text.txt"), {encoding: 'utf-8'});
+
+stream.on('readable', function() {
+  let data = stream.read();
+  if(data != null)console.log(data);
+})
+
