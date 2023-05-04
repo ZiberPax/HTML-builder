81bbe23e05ee025ed0817aa1a9b63842ec868d98
Alexey Meshin <ou3giu13hg@gmail.com>
2023-05-05 00:51:57 +0300

    feat: finished task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,33 @@
 
+const fs = require('fs');
+const path = require('path');
+const fsPromises = require('fs').promises;
+
+function copy () {
+  fs.readdir(path.join(__dirname, `files`), (err,files) => {
+    if (err) return console.error(err.message);
+    files.forEach(el => {
+      fsPromises.copyFile(path.join(__dirname, `files`,`${el}`), path.join(__dirname, `files-copy`,`${el}`))
+    })
+  })
+}
+
+
+fs.stat(path.join(__dirname, `files-copy`), err => {
+  if (!err) {
+    fs.readdir(path.join(__dirname, `files-copy`), (err, files) => {
+      if (err) return console.error(err.message);
+      files.forEach(el => {
+        fs.unlink(path.join(__dirname, `files-copy`,`${el}`), err => {
+          if (err) return console.error(err.message);
+        })
+      })
+    })
+    copy ()
+  }
+  else if (err.code === 'ENOENT') {
+    fs.promises.mkdir(path.join(__dirname, `files-copy`));
+    copy ()
+  }
+});
+
